91e57eb9814da61d6e0214d62bcba28c58c12528
Jason Flax <jsflax@gmail.com>
2026-06-07 15:02:35 -0400

    Lower min iOS version to v15

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import CompilerPluginSupport
 
 let package = Package(
     name: "Lattice",
-    platforms: [.macOS(.v14), .iOS(.v17)],
+    platforms: [.macOS(.v14), .iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
